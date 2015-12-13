e6d271c091a78baa1c795806d318e5030e791165
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-12-13 21:55:08 +0300

    Correct saveProfile endpoint

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '893500458498-jgdjakhanshp6nl2ee093jou591qqbmq.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
