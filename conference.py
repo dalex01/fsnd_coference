3ea3e8b29d44c64a3afe05bbb514733fbce53cdd
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-12-18 17:12:22 +0300

    Add filter to all conferences query

--- conference.py
+++ conference.py
@@ -47,6 +47,21 @@ DEFAULTS = {
     "seatsAvailable": 0,
     "topics": [ "Default", "Topic" ],
 }
+OPERATORS = {
+            'EQ':   '=',
+            'GT':   '>',
+            'GTEQ': '>=',
+            'LT':   '<',
+            'LTEQ': '<=',
+            'NE':   '!='
+            }
+
+FIELDS =    {
+            'CITY': 'city',
+            'TOPIC': 'topics',
+            'MONTH': 'month',
+            'MAX_ATTENDEES': 'maxAttendees',
+            }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -145,7 +160,7 @@ class ConferenceApi(remote.Service):
                 name='queryConferences')
     def queryConferences(self, request):
         """Query for conferences."""
-        conferences = Conference.query()
+        conferences = self._getQuery(request)
 
          # return individual ConferenceForm object per Conference
         return ConferenceForms(
@@ -186,16 +201,20 @@ class ConferenceApi(remote.Service):
         # q = q.filter(Conference.city == "Paris")
 
         # advanced filter building and usage
-        field = "city"
-        operator = "="
-        value = "London"
-        f = ndb.query.FilterNode(field, operator, value)
-        q = q.filter(f)
-        field = "topics"
-        operator = "="
-        value = "Medical Innovations"
-        f = ndb.query.FilterNode(field, operator, value)
-        q = q.filter(f)
+        #field = "city"
+        #operator = "="
+        #value = "London"
+        #f = ndb.query.FilterNode(field, operator, value)
+        #q = q.filter(f)
+        #field = "topics"
+        #operator = "="
+        #value = "Medical Innovations"
+        #f = ndb.query.FilterNode(field, operator, value)
+        #q = q.filter(f)
+        q = q.filter(Conference.city == "London")
+        q = q.filter(Conference.topics == "Medical Innovations")
+        q = q.order(Conference.name)
+        q = q.filter(Conference.month == 12)
 
         # TODO
         # add 2 filters:
@@ -205,6 +224,54 @@ class ConferenceApi(remote.Service):
         return ConferenceForms(
             items=[self._copyConferenceToForm(conf, "") for conf in q]
         )
+
+    def _getQuery(self, request):
+        """Return formatted query from the submitted filters."""
+        q = Conference.query()
+        inequality_filter, filters = self._formatFilters(request.filters)
+
+        # If exists, sort on inequality filter first
+        if not inequality_filter:
+            q = q.order(Conference.name)
+        else:
+            q = q.order(ndb.GenericProperty(inequality_filter))
+            q = q.order(Conference.name)
+
+        for filtr in filters:
+            if filtr["field"] in ["month", "maxAttendees"]:
+                filtr["value"] = int(filtr["value"])
+            formatted_query = ndb.query.FilterNode(filtr["field"], filtr["operator"], filtr["value"])
+            q = q.filter(formatted_query)
+        return q
+
+
+    def _formatFilters(self, filters):
+        """Parse, check validity and format user supplied filters."""
+        formatted_filters = []
+        inequality_field = None
+
+        for f in filters:
+            filtr = {field.name: getattr(f, field.name) for field in f.all_fields()}
+
+            try:
+                filtr["field"] = FIELDS[filtr["field"]]
+                filtr["operator"] = OPERATORS[filtr["operator"]]
+            except KeyError:
+                raise endpoints.BadRequestException("Filter contains invalid field or operator.")
+
+            # Every operation except "=" is an inequality
+            if filtr["operator"] != "=":
+                # check if inequality operation has been used in previous filters
+                # disallow the filter if inequality was performed on a different field before
+                # track the field on which the inequality operation is performed
+                if inequality_field and inequality_field != filtr["field"]:
+                    raise endpoints.BadRequestException("Inequality filter is allowed on only one field.")
+                else:
+                    inequality_field = filtr["field"]
+
+            formatted_filters.append(filtr)
+        return (inequality_field, formatted_filters)
+
 
 # - - - Profile objects - - - - - - - - - - - - - - - - - - -
 
